b77c97f1c22a899c0e8b9df357fcf1f29e021300
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2026-04-05 08:50:45 +0200

    Initial Commit

---
+++ jellyfin/jellyfinApp.swift
@@ -0,0 +1,17 @@
+//
+//  jellyfinApp.swift
+//  jellyfin
+//
+//  Created by neo on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct jellyfinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
