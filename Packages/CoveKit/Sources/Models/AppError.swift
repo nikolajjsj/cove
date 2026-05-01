cb1641415972f89bb00776bdb9e676f8f4f5e923
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2026-05-01 19:22:34 +0200

    refactor(app-error): tweak equatable conformance

--- Packages/CoveKit/Sources/Models/AppError.swift
+++ Packages/CoveKit/Sources/Models/AppError.swift
@@ -39,13 +39,67 @@ public enum AppError: Error, LocalizedError, Sendable {
 
     // Implement Equatable manually since `any Error` isn't Equatable
     public static func == (lhs: AppError, rhs: AppError) -> Bool {
-        lhs.localizedDescription == rhs.localizedDescription
+        switch (lhs, rhs) {
+        case (.networkUnavailable, .networkUnavailable):
+            return true
+        case (.serverUnreachable(let a), .serverUnreachable(let b)):
+            return a == b
+        case (.authExpired(let a), .authExpired(let b)):
+            return a == b
+        case (.authFailed(let a), .authFailed(let b)):
+            return a == b
+        case (.playbackFailed(let a), .playbackFailed(let b)):
+            return a == b
+        case (.downloadFailed(let a1, let a2), .downloadFailed(let b1, let b2)):
+            return a1 == b1 && a2 == b2
+        case (.storageFull, .storageFull):
+            return true
+        case (.itemNotFound(let a), .itemNotFound(let b)):
+            return a == b
+        case (.serverError(let a1, let a2), .serverError(let b1, let b2)):
+            return a1 == b1 && a2 == b2
+        case (.unknown, .unknown):
+            return lhs.localizedDescription == rhs.localizedDescription
+        default:
+            return false
+        }
     }
 }
 
 // Make Hashable for use in identifiable contexts
 extension AppError: Hashable {
     public func hash(into hasher: inout Hasher) {
-        hasher.combine(localizedDescription)
+        switch self {
+        case .networkUnavailable:
+            hasher.combine(0)
+        case .serverUnreachable(let url):
+            hasher.combine(1)
+            hasher.combine(url)
+        case .authExpired(let serverName):
+            hasher.combine(2)
+            hasher.combine(serverName)
+        case .authFailed(let reason):
+            hasher.combine(3)
+            hasher.combine(reason)
+        case .playbackFailed(let reason):
+            hasher.combine(4)
+            hasher.combine(reason)
+        case .downloadFailed(let itemTitle, let reason):
+            hasher.combine(5)
+            hasher.combine(itemTitle)
+            hasher.combine(reason)
+        case .storageFull:
+            hasher.combine(6)
+        case .itemNotFound(let id):
+            hasher.combine(7)
+            hasher.combine(id)
+        case .serverError(let statusCode, let message):
+            hasher.combine(8)
+            hasher.combine(statusCode)
+            hasher.combine(message)
+        case .unknown:
+            hasher.combine(9)
+            hasher.combine(localizedDescription)
+        }
     }
 }
