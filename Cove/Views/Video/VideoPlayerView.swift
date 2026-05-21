d956d7b9a8902a0b5db8c1f1f0cb8044e136ed13
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2026-05-21 12:12:45 +0200

    refactor(video-player): separate PlayerSeekSlider view

--- Cove/Views/Video/VideoPlayerView.swift
+++ Cove/Views/Video/VideoPlayerView.swift
@@ -523,48 +523,6 @@ struct VideoPlayerView: View {
         }
     }
 #endif
-// MARK: - Player Seek Slider
-
-/// The seek slider, isolated into its own view so that `videoManager.currentTime`
-/// reads happen here instead of in `VideoPlayerView.body`.
-#if !os(tvOS)
-    private struct PlayerSeekSlider: View {
-        let videoManager: VideoPlaybackManager
-        @Binding var isSeeking: Bool
-        @Binding var seekTime: TimeInterval
-        let isGestureSeeking: Bool
-        let gestureSeekTime: TimeInterval
-
-        private var displayTime: TimeInterval {
-            if isGestureSeeking { return gestureSeekTime }
-            if isSeeking { return seekTime }
-            return videoManager.currentTime
-        }
-
-        var body: some View {
-            Slider(
-                value: Binding(
-                    get: { displayTime },
-                    set: { newValue in
-                        if !isSeeking {
-                            isSeeking = true
-                        }
-                        seekTime = newValue
-                    }
-                ),
-                in: 0...max(videoManager.duration, 1),
-                onEditingChanged: { editing in
-                    if !editing {
-                        videoManager.seek(to: seekTime)
-                        isSeeking = false
-                    }
-                }
-            )
-            .tint(.white)
-        }
-    }
-#endif
-
 // MARK: - Player Time Labels
 
 /// Displays elapsed / total time, isolated so `currentTime` reads don't
